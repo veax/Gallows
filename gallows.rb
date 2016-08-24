a0a3946bd0cea5c8a89040cf82fb62ff12845823
Vadim Borisov <borissowadim@mail.ru>
2016-08-25 02:27:48 +0300

    initialize

---
+++ gallows.rb
@@ -0,0 +1,30 @@
+﻿require "./methods.rb"
+
+cls
+
+puts "игра виселица, версия 1"
+
+letters = get_letters
+
+errors = 0
+
+bad_letters = []
+good_letters = []
+
+while errors < 7 do
+	print_status(letters, good_letters, bad_letters, errors)
+
+	puts "введите след. букву"
+
+	user_input = get_user_input
+
+	result = check_result(user_input, letters, good_letters, bad_letters)
+
+	if (result == -1)
+		errors +=1
+	elsif (result == 1)
+		break
+	end
+end
+
+print_status(letters, good_letters, bad_letters, errors)
---
+++ methods.rb
@@ -0,0 +1,82 @@
+﻿def get_letters()   #возвращает загаданное слово в форме массива букв
+
+	slovo = ARGV[0]
+
+	if (Gem.win_platform? && ARGV[0])
+		slovo = slovo.encode(ARGV[0].encoding).encode("UTF-8")
+	end
+
+	if (slovo == nil || slovo == "")
+		abort "Вы не ввели слово для игры"
+	end
+
+	return slovo.split("") # .split разбивает введеное слово на массив из его букв
+end
+
+def get_user_input 
+	letter = ""
+
+	while letter == "" do
+		letter = STDIN.gets.encode("UTF-8").chomp
+	end
+
+	return letter
+end
+
+def check_result(user_input, letters, good_letters, bad_letters)
+	if (good_letters.include?(user_input) || bad_letters.include?(user_input)) #проверка на повторение буквы
+		return 0
+	end
+
+	if (letters.include?(user_input))
+		good_letters << user_input     # записываем введеную букву в массив good_letters
+
+		if  letters.uniq.size ==  good_letters.size #условие когда отгадано все слово
+			return 1
+		else
+			return 0
+		end
+	else
+		bad_letters << user_input
+		return -1
+	end
+end
+
+def get_word_for_print(letters, good_letters)
+	result = ""
+
+	for letter in letters do
+		if good_letters.include?(letter)
+			result += letter + " "
+		else
+			result += "__ "
+		end
+	end
+
+	return result
+end
+
+#1. выводить загаданное слово
+#2. информацию об ошибках и уже названные буквы
+#3. ошибок больше 7 - сообщить о поражении
+#4. слово угадано - сообщить о победе
+
+def print_status(letters, good_letters, bad_letters, errors)
+	puts "\n Слово: " + get_word_for_print(letters, good_letters) #1
+
+	puts "Ошибки (#{errors}: #{bad_letters.join(", ")})"
+
+	if errors >=7
+		puts "Вы проиграли :("
+	else
+		if letters.uniq.size ==  good_letters.size 
+			puts "Поздравляем! Вы выиграли! \n\n"
+		else
+			puts "У вас осталось попыток: " + (7-errors).to_s
+		end
+	end
+end
+
+def cls
+	system "clear" or system "cls"
+end
